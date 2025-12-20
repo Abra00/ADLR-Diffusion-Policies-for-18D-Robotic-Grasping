f8dd309598d6690f575ca40bc74c4a9f805e85d7
Luca Frontini <lfrontini@gmx.de>
2025-12-20 14:26:41 +0100

    chnaged the count parameter

--- train_sweep.py
+++ train_sweep.py
@@ -214,4 +214,4 @@ if __name__ == "__main__":
     with open("src/config.yaml") as f:
         sweep_config = yaml.safe_load(f)
     sweep_id = wandb.sweep(sweep_config, project="adlr-diffusion_grasping")
-    wandb.agent(sweep_id, train, count=5)
+    wandb.agent(sweep_id, train, count=20)
